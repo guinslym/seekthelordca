c36ea0241fbbb495f06aaa93aeb53041aeda2f6c
Guinslym <agmond@gmx.com.br>
2016-09-18 18:34:58 -0400

    urls

--- applications/websites/urls.py
+++ applications/websites/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     #About Us
     #url(r'^about/$', views.AboutUsTemplateView.as_view(), name='about'),
     #Create a url shortened POST+GET
-    url(r'*', views.homepage_and_create_shorturl, name='create'),
+    url(r'^', views.current_datetime, name='create'),
     #View the URL link
     #url(r'^url/(?P<slug>[0-9A-Za-z-]+)/$', views.detail_box, name='detail'),
 ]
